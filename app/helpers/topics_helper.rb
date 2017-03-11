0f78ff6d91c38af27a4abeb49f2d820996504a2e
janice <janicejiang.d@gmail.com>
2017-03-11 22:26:56 +0800

    implement order by votes

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,2 +1,9 @@
 module TopicsHelper
+  def render_down_vote_button(topic)
+    if topic.votes.count == 0
+      button_to '-1', "#", disabled: true # 按钮不可点击
+    else
+      button_to '-1', downvote_topic_path(topic), method: :post
+    end
+  end
 end
